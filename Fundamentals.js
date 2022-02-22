2cd883168842ce230a9e3fdadf0138b77dae6e30
Anastasia Kutsevolova <anastasiasemenok@gmail.com>
2022-02-21 20:14:06 -0700

    first commits

---
+++ Fundamentals.js
@@ -0,0 +1,23 @@
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const hieghtJohn = 1.95;
+
+const BMIMark = massMark/heightMark**2;
+const BMIJohn = massJohn/hieghtJohn**2;
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn);
+
+
+
+
+function user (mass,height){
+    const mark = ['78, 1.69'];
+    const john  ('92, 1.95');
+    const BMI = mass / height ** 2;
+}
+const mark = BMI;
+const john = BMI;
+console.log (mark);
